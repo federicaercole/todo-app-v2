ec6788d80b194bf2167e433b03f62f3869d93b64
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-07-04 15:17:12 +0200

    Add index route and view

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+//Routers
 const indexRouter = require('./routes/index');
 
 const app = express();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const sql = require('../models/dbConfig.js');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+async function getAllCategories() {
+  const [rows] = await sql.query("SELECT * FROM categories");
+  return rows;
+}
+
+async function getAllTodos() {
+  const [rows] = await sql.query("SELECT todo_id, title, due_date, priority, done, name AS category FROM todos JOIN categories ON todos.category = categories.cat_id");
+  return rows;
+}
+
+//Home page
+router.get('/', async function (req, res) {
+  const categories = await getAllCategories();
+  const todos = await getAllTodos();
+  res.render('index', { title: "All todos", categories: categories, todos: todos });
 });
 
 module.exports = router;
