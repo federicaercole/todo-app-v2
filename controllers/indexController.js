62648f5f4296ddcddcfa9877f614f506483ba3d2
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-07-05 17:23:40 +0200

    Update indes view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,8 +2,9 @@ const utilityFunctions = require('./utilityFunctions')
 
 async function getAll(req, res) {
     const categories = await utilityFunctions.getAllCategories();
+    const dates = await utilityFunctions.getTodoDates();
     const todos = await utilityFunctions.getAllTodos();
-    res.render('index', { title: "All todos", categories, todos });
+    res.render('index', { title: "All todos", categories, todos, dates });
 }
 
 module.exports = { getAll };
--- controllers/utilityFunctions.js
+++ controllers/utilityFunctions.js
@@ -5,9 +5,25 @@ async function getAllCategories() {
     return rows;
 }
 
+async function getTodoDates() {
+    const [rows] = await sql.query("SELECT DISTINCT due_date FROM todos ORDER BY due_date DESC");
+    const dates = rows.map(row => row.due_date);
+    return dates;
+}
+
+async function getSingleTodo(id) {
+    const [result] = await sql.query("SELECT done FROM todos WHERE todo_id = ?", id);
+    return result;
+}
+
 async function getAllTodos() {
-    const [rows] = await sql.query("SELECT todo_id, title, due_date, priority, done, name AS category FROM todos JOIN categories ON todos.category = categories.cat_id");
+    const [rows] = await sql.query("SELECT todo_id, title, due_date, priority, done, name AS category FROM todos JOIN categories ON todos.category = categories.cat_id ORDER BY due_date DESC");
     return rows;
 }
 
-module.exports = { getAllCategories, getAllTodos };
+async function getTodoStatus(todo_id) {
+    const [result] = await sql.query("SELECT done FROM todos WHERE todo_id = ? ", todo_id)
+    return result;
+}
+
+module.exports = { getAllCategories, getAllTodos, getTodoStatus, getTodoDates };
