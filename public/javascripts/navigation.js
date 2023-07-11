617f4459a5c7144a70aba5bd8a21accd5e419b8e
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-07-11 17:11:00 +0200

    Add menu script and style

---
+++ public/javascripts/navigation.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector("#menu");
+const span = menuBtn.querySelector("span");
+const nav = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    if (!nav.classList.contains("show-menu")) {
+        nav.classList.add("show-menu");
+        span.textContent = "Close menu";
+        menuBtn.setAttribute("aria-expanded", "true");
+    } else {
+        menuBtn.setAttribute("aria-expanded", "false");
+        nav.classList.remove("show-menu");
+        span.textContent = "Open menu";
+    }
+});
+
