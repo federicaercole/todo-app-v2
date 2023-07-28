3960f360f8378573949177757e7570696722eee6
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-07-28 16:10:28 +0200

    Change database configuration

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
+    port: process.env.PORT,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
 });
