8f1f962e6099b850fcfb887b767b27dd7c2e5064
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-08-24 16:12:19 +0200

    Refactor error page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/indexController');
+const { signIn } = require('../controllers/userController');
 
 router.get('/', indexController.getHome);
 router.get('/sign-up', indexController.userSignUpGet);
 router.get('/sign-in', indexController.userSignInGet);
 router.get("/logout", indexController.userLogout);
+router.get('/demo', indexController.demoUserSignIn, signIn);
 
 module.exports = router;
