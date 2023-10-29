e1333f699d674fcae1ee7616c2570b470ad10f7f
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-10-29 12:34:42 +0100

    Fix validation for signup page

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,5 +1,3 @@
-import { checkIfItemExistsInDOM } from "./utility.js";
-
 const errorIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"
 focusable="false">
 <path fill="currentColor"
@@ -10,10 +8,10 @@ const form = document.querySelector("form");
 
 const catFields = {
     name: {
-        input: form.querySelector("input[name='name']"),
-        error: document.querySelector(".info.error.hidden"),
+        getInput() { return document.querySelector("input[name='name']") },
+        getError() { return document.querySelector("#cat-err") },
         errorMsg: "Category name must be between 3 and 30 characters",
-        isFieldValid() { return this.input.validity.valid },
+        isFieldValid() { return this.getInput().validity.valid },
     }
 };
 
@@ -22,11 +20,13 @@ const todoFields = {
         input: document.querySelector("input[name='title']"),
         error: document.querySelector("#title-err"),
         errorMsg: "To-do title must be between 1 and 100 characters",
+        isFieldValid() { return this.input.validity.valid },
     },
     date: {
         input: document.querySelector("input[name='due_date']"),
         error: document.querySelector("#date-err"),
         errorMsg: "You must insert a valid due date",
+        isFieldValid() { return this.input.validity.valid },
     }
 };
 
@@ -35,49 +35,65 @@ const userFields = {
         input: document.querySelector("input[name='email']"),
         error: document.querySelector("#email-err"),
         errorMsg: "Must be a valid email address",
+        isFieldValid() { return this.input.validity.valid },
     },
     password: {
         input: document.querySelector("input[name='password']"),
         error: document.querySelector("#password-err"),
         errorMsg: "You must provide a password",
-    },
-    confirmPassword: {
-        input: document.querySelector("input[name='confirm-password']"),
-        error: document.querySelector("#password-err"),
-        errorMsg: "Passwords do not match",
+        isFieldValid() { return this.input.validity.valid },
     }
 };
 
-const fields = [catFields, todoFields, userFields];
+const signupFields = {
+    confirmPassword: {
+        input: document.querySelector("input[name='confirm-password']"),
+        error: document.querySelector("#password-conf-err"),
+        errorMsg: "Passwords do not match",
+        isFieldValid() { return this.input.value === userFields.password.input.value },
+    }
+};
 
-form.addEventListener("submit", formValidation);
+const fields = [catFields, todoFields, userFields, signupFields];
 
-export function formValidation(event) {
+form.addEventListener("submit", formValidation());
 
-    const [fieldsInPage] = fields.filter(item => {
+export function formValidation() {
+    const fieldsInPage = fields.filter(item => {
         for (const field in item) {
-            return checkIfItemExistsInDOM(item[field])
+            return item[field].input;
         }
     });
+    return function preventSubmission(event) {
+        fieldsInPage.forEach(field => {
+            if (!validateForm(field)) {
+                event.preventDefault();
+            }
+        });
+    }
+}
 
-    const isFormValid = Object.keys(fieldsInPage).reduce((acc, curr) => {
-        const currentField = fieldsInPage[curr];
+function validateField(field) {
+    const isFieldValid = field.isFieldValid();
+    return function setErrorDOM() {
+        field.input.setAttribute("aria-invalid", !isFieldValid);
+        isFieldValid ? field.error.classList.add("hidden") : field.error.classList.remove("hidden");
+        isFieldValid ? field.error.innerHTML = "" : field.error.innerHTML = `${errorIcon} ${field.errorMsg}`;
+        return isFieldValid;
+    }
+}
 
-        if (currentField.isFieldValid()) {
+function validateForm(fields) {
+    const isFormValid = Object.keys(fields).reduce((acc, curr) => {
+        const currentField = fields[curr];
+        const isFieldValid = validateField(currentField);
+
+        if (isFieldValid()) {
             return acc;
         } else {
-            showError(currentField);
             return false;
         }
     }, true);
 
-    if (!isFormValid) {
-        event.preventDefault();
-    }
-}
-
-function showError(field) {
-    field.input.setAttribute("aria-invalid", "true");
-    field.error.classList.remove("hidden");
-    field.error.innerHTML = `${errorIcon} ${field.errorMsg}`;
+    return isFormValid;
 }
