4e08835c38201f6750888fe6e760c1f246d473a7
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2024-07-18 12:35:30 +0200

    Improve accessibility in views

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,11 +4,21 @@ import { manageForm } from "./validation.js";
 manageBtnEvents();
 filterMenuDOM();
 highlightCurrentPage();
+focusInfoMessageBtn();
 manageForm();
 
 function highlightCurrentPage() {
     const currentNavElement = document.querySelector(`a[href="${window.location.pathname}"`);
     if (currentNavElement) {
         return currentNavElement.setAttribute("aria-current", "page");
+    }
+}
+
+function focusInfoMessageBtn() {
+    const infoMessage = document.querySelector("aside.info");
+    if (infoMessage) {
+        infoMessage.setAttribute("aria-live", "polite");
+        const infoMessageBtn = infoMessage.querySelector(".close");
+        infoMessageBtn.focus();
     }
 }
